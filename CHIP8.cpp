f2f25add5ccab34f8d72b7777cf94c2437a8b02f
Josh Matulenas <joshmatulenas@Joshs-Air.fios-router.home>
2021-06-21 20:54:34 -0400

    Update CHIP8

--- CHIP8.cpp
+++ CHIP8.cpp
@@ -39,7 +39,7 @@ typedef struct Chip8 {
     +---------------+= 0x000 (0) Start of Chip-8 RAM */
     
     //  Our 4kb of ram
-    uint8_t ram[4096];
+    uint8_t ram[4096] = {};
 
     //  Chip-8 has 16 general purpose 8-bit registers, usually referred to as Vx, where
     //  x is a hexadecimal digit (0 through F). There is also a 16-bit register called
@@ -64,28 +64,28 @@ typedef struct Chip8 {
     //	up to 16 levels of nested subroutines.
 
     // Registers V0 -- VF
-    uint8_t V[16];
+    uint8_t V[16] = {};
 
     // 16 bit Index register
-    uint16_t I;
+    uint16_t I = 0x0000;
 
     // Program Counter
-    uint16_t PC;
+    uint16_t PC = 0x0000;
 
     // Opcode Scheme:
-    //                   ++++--:Operation Select
-    //                   ||||
-    //                   ||||++++--Vx: Regsister Select
-    //                   ||||||||
-    //                   ||||||||++++--Vy: Resgister Select
-    //                   ||||||||||||
-    //                   ||||||||||||++++--N: 4 bit number / nibble
-    //                   ||||||||||||||||
-    char16_t instr; // 0b0000000000000000;
-    //                       ||||||||||||
-    //                       ||||++++++++--NN: Second Byte
-    //                       ||||
-    //                       ++++--NNN: 2nd 3rd and 4th nibbles
+    //                 ++++--:Operation Select
+    //                 ||||
+    //                 ||||++++--Vx: Regsister Select
+    //                 ||||||||
+    //                 ||||||||++++--Vy: Resgister Select
+    //                 ||||||||||||
+    //                 ||||||||||||++++--N: 4 bit number / nibble
+    //                 ||||||||||||||||
+    char16_t instr = 0b0000000000000000;
+    //                     ||||||||||||
+    //                     ++++++++++++--NN: 2nd 3rd and 4th nibble
+    //                         ||||||||
+    //                         ++++++++--NNN: Second Byte
     #define OP  0xF000
     #define Vx  0x0F00
     #define Vy  0x00F0
@@ -94,19 +94,19 @@ typedef struct Chip8 {
     #define NNN 0x0FFF
 
     // Stack Pointer
-    uint8_t SP;
+    uint8_t SP = 0x00;
 
     // Stack
-    uint16_t stack[16];
+    uint16_t stack[16] = {};
 
     // Display -- 64x32 pixels
-    bool display[64][32];
+    bool display[64][32] = {};
     
     // 8  bit Delay timer register @60Hz
-    uint8_t dTimer;
+    uint8_t dTimer = 0x00;
 
     // 8 bit sound timer register @60Hz
-    uint8_t sTimer;
+    uint8_t sTimer = 0x00;
 
     // Fontset -- "Cosmacvip"
     uint8_t fontset[80] = {
@@ -153,7 +153,11 @@ void loadROM(char const* filename, Chip8* vm) {
 }
 // Grab opcode, Increment Program Counter
 void fetch (Chip8* vm) {
+    // Grab first byte of instruction
     vm->instr = vm->ram[vm->PC];
+    // Shift and grab second byte of instruction
+    vm->instr = (vm->instr<<8) | vm->ram[(vm->PC+1)]; 
+    // Increment Program Counter
     vm->PC += 2;
 }
 
@@ -170,20 +174,20 @@ void draw (const uint8_t* x, const uint8_t* y, const uint8_t* n, const uint16_t*
 }
 #define INSTRUCTION_LIST(o)\
     o("SYS addr",           "0nnn", )/*Execute machine language subroutine at address NNN*/\
-    o("CLS",                "00E0", u == 0x0 && kk == 0xE0, reset(display))/*Clear the screen*/\
+    o("CLS",                "00E0", u == 0x0 && kk == 0xE0, reset(vm->display))/*Clear the screen*/\
     o("RET",                "00EE", u == 0x0 && kk == 0xEE, )/*Return from a subroutine*/\
-    o("JP addr",            "1nnn", u == 0x1, PC = nnn)/*Jump to address NNN*/\
+    o("JP addr",            "1nnn", u == 0x1, vm->PC = nnn)/*Jump to address NNN*/\
     o("CALL addr",          "2nnn", u == 0x2, )/*Execute subroutine starting at address NNN*/\
-    o("SE Vx, byte",        "3xkk", u == 0x3, if (V[x] == nn)  PC += 2 )/*Skip the following instruction if the value of register VX equals NN*/\
-    o("SNE Vx, byte",       "4xkk", u == 0x4, if (V[x] != nn)  PC += 2 )/*Skip the following instruction if the value of register VX is not equal to NN*/\
-    o("SE Vx, Vy",          "5xy0", u == 0x5, if (V[x] == V[y] PC += 2 ))/*Skip the following instruction if the value of register VX is equal to the value 
+    o("SE Vx, byte",        "3xkk", u == 0x3, if (vm->V[x] == nn)  vm->PC += 2 )/*Skip the following instruction if the value of register VX equals NN*/\
+    o("SNE Vx, byte",       "4xkk", u == 0x4, if (vm->V[x] != nn)  vm->PC += 2 )/*Skip the following instruction if the value of register VX is not equal to NN*/\
+    o("SE Vx, Vy",          "5xy0", u == 0x5, if (vm->V[x] == V[y] vm->PC += 2 ))/*Skip the following instruction if the value of register VX is equal to the value 
                                                             of register VY*/\
     o("LD Vx, byte",        "6xkk", u == 0x6, V[x] = kk)/*Store number NN in register VX*/\
     o("ADD Vx, byte",       "7xkk", u == 0x7, V[x] += kk)/*Add the value NN to register VX*/\
-    o("LD Vx, Vy",          "8xy0", u == 0x8 && n == 0x0, V[x] = V[y])/*Store the value of register VY in register VX*/\
-    o("OR Vx, Vy",          "8xy1", u == 0x8 && n == 0x1, V[x] = V[x] | V[y])/*Set VX to VX OR VY*/\
-    o("AND Vx, Vy",         "8xy2", u == 0x8 && n == 0x2, V[x] = V[x] & V[y])/*Set VX to VX AND VY*/\
-    o("XOR Vx, Vy",         "8xy3", u == 0x8 && n == 0x3, V[x] = V[x] ^ V[y])/*Set VX to VX XOR VY*/\
+    o("LD Vx, Vy",          "8xy0", u == 0x8 && n == 0x0, vm->V[x] = vm->V[y])/*Store the value of register VY in register VX*/\
+    o("OR Vx, Vy",          "8xy1", u == 0x8 && n == 0x1, vm->V[x] = vm->V[x] | vm->V[y])/*Set VX to VX OR VY*/\
+    o("AND Vx, Vy",         "8xy2", u == 0x8 && n == 0x2, vm->V[x] = vm->V[x] & vm->V[y])/*Set VX to VX AND VY*/\
+    o("XOR Vx, Vy",         "8xy3", u == 0x8 && n == 0x3, vm->V[x] = vm->V[x] ^ vm->V[y])/*Set VX to VX XOR VY*/\
     o("ADD Vx, Vy",         "8xy4", u == 0x8 && n == 0x4, )/*Add the value of register VY to register VX
                                                             Set VF to 01 if a carry occurs
                                                             Set VF to 00 if a carry does not occur*/\
@@ -199,21 +203,21 @@ void draw (const uint8_t* x, const uint8_t* y, const uint8_t* n, const uint16_t*
     o("SHL Vx {, Vy}",      "8xyE", u == 0x8 && n == 0xE, )/*Store the value of register VY shifted left one bit in register VX¹
                                                             Set register VF to the most significant bit prior to the shift
                                                             VY is unchanged*/\
-    o("SNE Vx, Vy",         "9xy0", u == 0x9, )/*Skip the following instruction if the value of register VX is not equal to the 
+    o("SNE Vx, Vy",         "9xy0", u == 0x9, if (vm->V[x] != vm->V[y]) { vm->PC += 2 })/*Skip the following instruction if the value of register VX is not equal to the 
                                                            value of register VY*/\
-    o("LD I, addr",         "Annn", u == 0xA , I = nnn)/*Store memory address NNN in register I*/\
+    o("LD I, addr",         "Annn", u == 0xA , vm->I = nnn)/*Store memory address NNN in register I*/\
     o("JP V0, addr",        "Bnnn", u == 0xB, )/*Jump to address NNN + V0*/\
     o("RND Vx, byte",       "Cxkk",)/*Set VX to a random number with a mask of NN*/\
-    o("DRW Vx, Vy, nibble", "Dxyn", u == 0xD, display(x, y, n, I)) /*Draw a sprite at position VX, VY with N bytes of sprite data starting at the 
+    o("DRW Vx, Vy, nibble", "Dxyn", u == 0xD, draw (x, y, n, I)) /*Draw a sprite at position VX, VY with N bytes of sprite data starting at the 
                                                             address stored in I
                                                             Set VF to 01 if any set pixels are changed to unset, and 00 otherwise*/\
     o("SKP Vx",             "Ex9E", u == 0xE, )/*Skip the following instruction if the key corresponding to the hex value 
                                                            currently stored register VX is pressed*/\
-    o("LD Vx, DT",          "Fx07", u == 0xF && kk == 0x07, )/*Store the current value of the delay timer in register VX*/\
+    o("LD Vx, DT",          "Fx07", u == 0xF && kk == 0x07, vm->V[x] = vm->dTimer)/*Store the current value of the delay timer in register VX*/\
     o("LD Vx, K",           "Fx0A", u == 0xF && kk == 0x0A, )/*Wait for a keypress and store the result in register VX*/\
-    o("LD DT, Vx",          "Fx15", u == 0xF && kk == 0x15, )/*Set the delay timer to the value of register VX*/\
-    o("LD ST, Vx",          "Fx18", u == 0xF && kk == 0x18, )/*Set the sound timer to the value of register VX*/\
-    o("ADD I, Vx",          "Fx1E", u == 0xF && kk == 0x1E, V[x] += I)/*Add the value stored in register VX to register I*/\
+    o("LD DT, Vx",          "Fx15", u == 0xF && kk == 0x15, vm->dTimer = vm->V[x])/*Set the delay timer to the value of register VX*/\
+    o("LD ST, Vx",          "Fx18", u == 0xF && kk == 0x18, vm->sTimer = vm->V[x])/*Set the sound timer to the value of register VX*/\
+    o("ADD I, Vx",          "Fx1E", u == 0xF && kk == 0x1E, vm->V[x] += vm->I)/*Add the value stored in register VX to register I*/\
     o("LD F, Vx",           "Fx29", u == 0xF && kk == 0x29, )/*Set I to the memory address of the sprite data corresponding to the hexadecimal 
                                                            digit stored in register VX*/\
     o("LD B, Vx",           "Fx33", u == 0xF && kk == 0x33, )/*Store the binary-coded decimal equivalent of the value stored in register VX at 
@@ -224,8 +228,6 @@ void draw (const uint8_t* x, const uint8_t* y, const uint8_t* n, const uint16_t*
                                                            address I. I is set to I + X + 1 after operation*/
 
 void decode (Chip8* vm) {
-    vm->instr = vm->ram[vm->PC & 0xFFF]*0x100 + vm->ram[(vm->PC+1) & 0xFFF]; 
-
     unsigned u   = vm->instr & OP; // u - First 4 bits of instruction
     unsigned x   = vm->instr & Vx; // x - A 4-bit value, the lower 4 bits of the high byte of the instruction
     unsigned y   = vm->instr & Vy; // y - A 4-bit value, the upper 4 bits of the low byte of the instruction
@@ -238,10 +240,11 @@ void decode (Chip8* vm) {
     #undef o
 }
 
-void initVM (Chip8* vm, const char* ROMfilename) {
-    // Initialize PC to point to the 0x200 position in RAM
+void initVM (Chip8* vm, const char* ROMfile) {
+    
+    // Initialize PC to the 0x200 position in RAM
     vm->PC = 0x200;
-    loadROM(ROMfilename, vm);
+    loadROM(ROMfile, vm);
 }
 
 int main(int argc, char** argv) {
